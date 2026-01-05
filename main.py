8a1def7dcdc6d294b101e501688b61a4f2b6527c
Junya <junjia.liu@pm.me>
2026-01-05 00:55:35 +0000

    setup socket handler

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import os
+import pytz
+from dotenv import load_dotenv
+from config import USER_ID, CHANNEL_ID, TIMEZONE
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+load_dotenv()
+
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+
+TIMEZONE = pytz.timezone(TIMEZONE)
+
 def main():
-    pass
+    handler = SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN"))
+    handler.start()
 
 if __name__ == '__main__':
     main()
